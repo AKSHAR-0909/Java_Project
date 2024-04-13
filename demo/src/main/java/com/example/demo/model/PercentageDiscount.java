dfdb04b6beedcf6966faf152fd769b914d7e884b
AdithyaMahesh <adithyamahesh2003@gmail.com>
2024-04-13 19:43:47 +0530

    percentage discount class filled

--- demo/src/main/java/com/example/demo/model/PercentageDiscount.java
+++ demo/src/main/java/com/example/demo/model/PercentageDiscount.java
@@ -1,1 +1,14 @@
 package com.example.model;
+
+public class PercentageDiscount implements Discount {
+    private float percentage;
+
+    public PercentageDiscount(float percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public float applyDiscount(float amount) {
+        return amount * (1 - percentage / 100);
+    }
+}

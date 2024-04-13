d37817d421ea5df110a169e42e3e86b4f1c672af
AdithyaMahesh <adithyamahesh2003@gmail.com>
2024-04-13 19:42:17 +0530

    fixed discount class filled

--- demo/src/main/java/com/example/demo/model/FixedDiscount.java
+++ demo/src/main/java/com/example/demo/model/FixedDiscount.java
@@ -1,2 +1,14 @@
 package com.example.model;
 
+public class FixedDiscount implements Discount {
+    private float discountAmount;
+
+    public FixedDiscount(float discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    public float applyDiscount(float amount) {
+        return amount - discountAmount;
+    }
+}

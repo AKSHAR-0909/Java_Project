d3a00730a32237c80c32b22f8eab11e27773b8dd
AdithyaMahesh <adithyamahesh2003@gmail.com>
2024-04-13 19:51:40 +0530

    discount controller set up with necessary imports

---
+++ demo/src/main/java/com/example/demo/controller/DiscountController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Discount;
+import com.example.demo.service.DiscountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+

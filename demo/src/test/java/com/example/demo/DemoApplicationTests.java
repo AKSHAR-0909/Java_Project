3f5a8ee10aa620e06ffc394f9a566df6b56e33c7
AdithyaMahesh <adithyamahesh2003@gmail.com>
2024-04-13 22:57:11 +0530

    Changes in discount feature

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,6 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled("Temporarily disabled due to failure")
+public class MyTest {
+
+    @Test
+    public void myFailingTest() {
+        // Test logic
+    }
+
+    @Test
+    public void anotherFailingTest() {
+        // Test logic
+    }
+}
 
 @SpringBootTest
 class DemoApplicationTests {
